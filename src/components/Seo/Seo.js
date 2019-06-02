48283d5be5de8353f3b8a60fae7bc29ebe938624
David Asaf <asaf.david.e@gmail.com>
2019-06-02 18:41:14 -0400

    Small SEO Updates

--- src/components/Seo/Seo.js
+++ src/components/Seo/Seo.js
@@ -1,7 +1,7 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Helmet from "react-helmet";
-import config from "../../../content/meta/config";
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import config from '../../../content/meta/config';
 
 const Seo = props => {
   const { data, facebook } = props;
@@ -19,7 +19,7 @@ const Seo = props => {
     <Helmet
       htmlAttributes={{
         lang: config.siteLanguage,
-        prefix: "og: http://ogp.me/ns#"
+        prefix: 'og: http://ogp.me/ns#'
       }}
     >
       {/* General tags */}
@@ -31,13 +31,13 @@ const Seo = props => {
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
       <meta property="og:type" content="website" />
-      <meta property="fb:app_id" content={facebook.appId} />
+      {/* <meta property="fb:app_id" content={facebook.appId} /> */}
       {/* Twitter Card tags */}
-      <meta name="twitter:card" content="summary" />
+      {/* <meta name="twitter:card" content="summary" />
       <meta
         name="twitter:creator"
         content={config.authorTwitterAccount ? config.authorTwitterAccount : ""}
-      />
+      /> */}
     </Helmet>
   );
 };
