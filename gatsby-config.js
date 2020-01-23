87b170c86876688d0ecfcd5e70435c9121e8196d
David Asaf <asaf.david.e@gmail.com>
2020-01-22 21:36:58 -0500

    updating google tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -196,17 +196,17 @@ module.exports = {
 
         // Include GTM in development.
         // Defaults to false meaning GTM will only be loaded in production.
-        includeInDevelopment: false,
+        includeInDevelopment: false
 
         // datalayer to be set before GTM is loaded
         // should be an object or a function that is executed in the browser
         // Defaults to null
-        defaultDataLayer: { platform: 'gatsby' },
+        // defaultDataLayer: { platform: 'gatsby' },
 
         // Specify optional GTM environment details.
-        gtmAuth: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING',
-        gtmPreview: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME',
-        dataLayerName: 'YOUR_DATA_LAYER_NAME'
+        // gtmAuth: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING',
+        // gtmPreview: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME',
+        // dataLayerName: 'YOUR_DATA_LAYER_Nyarn AME'
       }
     },
     {
